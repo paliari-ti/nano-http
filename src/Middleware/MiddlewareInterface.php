06a06b23cf0ef74b9ccaa728af50f85c060b56e5
Marcos Paliari <marcos@paliari.com.br>
2020-05-02 18:41:12 -0300

    doc block

--- src/Middleware/MiddlewareInterface.php
+++ src/Middleware/MiddlewareInterface.php
@@ -2,7 +2,20 @@
 
 namespace Paliari\NanoHttp\Middleware;
 
+use Paliari\NanoHttp\Http\Request;
+use Paliari\NanoHttp\Http\Response;
+
 interface MiddlewareInterface
 {
+    /**
+     * @param Request  $request
+     * @param Response $response
+     * @param callable $next Any callable that accepts three arguments:
+     *                           1. A Request object
+     *                           2. A Response object
+     *                       return Response object
+     *
+     * @return Response
+     */
     public function __invoke($request, $response, $next);
 }
